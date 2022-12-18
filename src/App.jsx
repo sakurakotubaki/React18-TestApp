f85d06b729fd9ad0b394f5bb3bd47a76e1111f66
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-12-18 10:11:27 +0900

    fix App.jsxをESlintの設定に合わせる

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
 function App() {
-
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <div className="App">
       <h1>Hello World</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
